44b17685f1d0609009df08ebc9f4a807c123ae17
evgenKorobkov <evgenKorobkov@gmail.com>
2014-03-09 16:38:47 +0400

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChuvsuServer::Application.config.secret_key_base = 'e539eb45937c86f7fc6fccf06904118a4828c216b6bbe219f7d6da9b9a29b274d2d9d7a59d5d69f76979dbcf9c350352a04c3161f79e3adbc749c9aaf0e68597'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChuvsuServer::Application.config.session_store :cookie_store, key: '_chuvsu_server_session'
