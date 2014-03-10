d3ad407979b2649a7893951a82f31fad77cb2112
evgeny korobkov <me@evgenkorkov.ru>
2014-03-10 19:46:31 +0400

    новости не добавляется если уже есть

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,14 +1,14 @@
 class NewsItem < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
-  #validates :url, presence: true
+  validates :url, presence: true
 
 
   def self.update_news
     url = "http://www.chuvsu.ru/index.php?option=com_content&view=section&layout=blog&id=97&Itemid=139&format=feed&type=rss"
     rss_feed = parse_rss url
     rss_feed.items.each do |item|
-      NewsItem.create title: item.title, body: item.description#, url: item.url
+      NewsItem.create title: item.title, body: item.description, url: item.link unless is_dublicate?(item.link)
     end
   end
 
@@ -18,4 +18,10 @@ private
     fp = FeedParser.new url: url
     fp.parse
   end
+
+  def self.is_dublicate? link
+    result = find_by url: link
+    return not(result.nil?)
+  end
+
 end
