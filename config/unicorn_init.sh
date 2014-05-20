30f58c11f30151ac5572716e9eeb00bb0ada2308
evgeny korobkov <evgenkorobkov@gmail.com>
2014-05-20 08:14:56 +0400

    fix config nginx unicorn

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/chuvsu_server/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
 
