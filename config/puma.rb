d70dacd45cfd855a93cbffe5e9601a97bc95f9e0
evgeny korobkov <evgenkorobkov@gmail.com>
2014-08-17 00:43:45 +0400

    save connect active record

--- config/puma.rb
+++ config/puma.rb
@@ -9,3 +9,9 @@ state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
 stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
 threads 0, 16
 bind "unix://#{application_path}/tmp/sockets/#{railsenv}.socket"
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
