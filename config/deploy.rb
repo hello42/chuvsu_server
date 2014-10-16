694bc10961c3aaa111307530f518fc9034015aef
evgeny korobkov <evgenkorobkov@gmail.com>
2014-10-16 21:06:10 +0400

    Добавил: при деплое загруженные файлы сохранются тоже

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,8 @@ set :deploy_to, '/var/www/chuvsu'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
 #set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
 #set :puma_init_active_record, true
 #set :puma_state, "#{shared_path}/tmp/pids/puma-#{fetch(:stage)}.state"
